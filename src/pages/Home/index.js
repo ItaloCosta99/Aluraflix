c2d0226bd6d45f852749be9d70f1195d8e5c30ba
Italo Costa <italocostajobs@gmail.com>
2020-07-28 23:49:25 -0300

    Added Cadastro base and Carousel Effect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
-    <div style={{ background: "#141414"}}>
+    <div style={{ background: "#141414" }}>
       <Menu />
+
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
@@ -45,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
